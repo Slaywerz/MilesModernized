e302b006de08df31b055877b31265dcdedd6eae4
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-03-15 18:41:23 +0500

    Variables name changed

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Пожалуйста, введите стоимость вашего билета: ");
-        int ticket_price = in.nextInt();
+        int ticketPrice = in.nextInt();
         int divider = 20;
-        int bonus_miles = ticket_price / divider;
-        System.out.println(bonus_miles);
+        int bonusMiles = ticketPrice / divider;
+        System.out.println("За вашу покупку вам начислено " + bonusMiles + " миль.");
     }
 }
