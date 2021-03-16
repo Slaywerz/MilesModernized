210efe981983b4ac3ddfae4003cfdca37e631b58
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-03-17 01:42:09 +0500

    Payment bonus miles modified

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
         Scanner in = new Scanner(System.in);
         System.out.print("Пожалуйста, введите стоимость вашего билета: ");
-        int ticketPrice = in.nextInt();
-        int divider = 20;
-        int bonusMiles = ticketPrice / divider;
+        int cost = in.nextInt();
+        int bonusMiles = service.calculate(cost);
         System.out.println("За вашу покупку вам начислено " + bonusMiles + " миль.");
     }
 }
