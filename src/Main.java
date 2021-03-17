18432be7bb5ebdbdb50fdce9f24aceb0e048712a
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-03-17 11:38:19 +0500

    fixes

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         BonusMilesService service = new BonusMilesService();
         Scanner in = new Scanner(System.in);
         System.out.print("Пожалуйста, введите стоимость вашего билета: ");
-        int cost = in.nextInt();
-        int bonusMiles = service.calculate(cost);
+        int price = in.nextInt();
+        int bonusMiles = service.calculate(price);
         System.out.println("За вашу покупку вам начислено " + bonusMiles + " миль.");
     }
 }
